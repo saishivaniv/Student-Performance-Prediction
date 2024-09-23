dbc860aab2bc8fe9367be0c926b706069e06fe0c
Sai Shivani V <saishivani.v@gmail.com>
2024-09-23 23:02:08 +0530

    Hyperparameter Training

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -28,11 +28,11 @@ class DataTransformation:
         This function is responsible for data transformation
         '''
         try:
-            numerical_columns = ['writing score','reading score']
+            numerical_columns = ['writing_score','reading_score']
             categorical_columns = [
-                'gender','race/ethnicity',
-                'parental level of education','lunch',
-                'test preparation course'
+                'gender','race_ethnicity',
+                'parental_level_of_education','lunch',
+                'test_preparation_course'
             ]
             
             #creating pipeline
@@ -77,8 +77,8 @@ class DataTransformation:
             
             preprocessing_obj=self.get_data_transformer_object()
             
-            target_column_name='math score'
-            numerical_columns=['writing score','reading score']
+            target_column_name='math_score'
+            numerical_columns=['writing_score','reading_score']
             
             input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df=train_df[target_column_name]
